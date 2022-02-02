f3ef1cb5a0f443a83d1fe9610d81daca0bc97102
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-02-02 20:37:52 -0300

    bonus

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -9,6 +9,7 @@ const {
   REACT_APP_HAWKINS_TIMEOUT,
   REACT_APP_UPSIDEDOWN_URL,
   REACT_APP_UPSIDEDOWN_TIMEOUT,
+  REACT_APP_DEVELOPMENT,
 } = process.env;
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
@@ -124,6 +125,7 @@ class StrangerThings extends React.Component {
       >
         <div className="content strangerfy">
           <div className="change-reality">
+            <p>{REACT_APP_DEVELOPMENT}</p>
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
               Mudar de Realidade
